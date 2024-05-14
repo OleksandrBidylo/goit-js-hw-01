b9966052ddf8109b86c580aff522a4c227cf1ce2
OleksandrBidylo <161107668+OleksandrBidylo@users.noreply.github.com>
2024-05-14 21:20:12 +0300

    fixed

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,7 @@
 function getShippingMessage(country, price, deliveryFee) {
   return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 }
+
+console.log(getShippingMessage("Australia", 120, 50));
+console.log(getShippingMessage("Canada", 200, 30));
+console.log(getShippingMessage("France", 80, 20));
